5ad7898e5778dc14848342c5983f74ff44745131
Eduphore <alemantrixaryan@gmail.com>
2022-08-11 05:29:07 +0530

    nightmare also failed

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const {
   CollectContent,
 } = require("nodejs-web-scraper");
 const fs = require("fs");
+const Nightmare = require("nightmare");
 
 // npm i nodejs-web-scraper   --> This package will solve my issue
 
@@ -24,38 +25,14 @@ const port = process.env.PORT || "8085";
 
 // GET /
 app.get("/", async (req, res) => {
-  let url =
-    "https://ecomm.one-line.com/one-ecom/manage-shipment/cargo-tracking?ctrack-field=BMOU4877725&trakNoParam=BMOU4877725";
   try {
+    const url =
+      "https://ecomm.one-line.com/one-ecom/manage-shipment/cargo-tracking?ctrack-field=BMOU4877725&trakNoParam=BMOU4877725";
+
     const response = await axios.get(url);
     if (response) {
-      const pages = []; //All ad pages.
-      const getPageObject = (pageObject, address) => {
-        pages.push(pageObject);
-      };
-
-      // handle your response
-
-      (async () => {
-        const config = {
-          baseSiteUrl: `https://ecomm.one-line.com/`,
-          startUrl: url,
-          concurrency: 10,
-          maxRetries: 3,
-          logPath: "./logs/",
-        };
-        const scraper = new Scraper(config); //Create a new Scraper instance, and pass config to it.
-        const root = new Root();
-        const status = new CollectContent("h2", {
-          name: "status",
-          getPageObject,
-        });
-        root.addOperation(status);
-        await scraper.scrape(root);
-        fs.writeFile("./test.json", JSON.stringify(pages), () => {});
-      })();
+      res.json(response.data);
     }
-    res.json(response.data);
   } catch (e) {
     console.log(e);
     res.send("eror");
@@ -78,3 +55,49 @@ app.listen(port, () => {
 // for (let td of CSSselect.selectAll("h1", response.data)) {
 //   console.log(`td ${render(td)}`);
 // }
+
+// const pages = []; //All ad pages.
+// const getPageObject = (pageObject, address) => {
+//   pages.push(pageObject);
+// };
+
+// handle your response
+
+// (async () => {
+//   const config = {
+//     baseSiteUrl: `https://www.google.com/`,
+//     startUrl: url,
+//     concurrency: 10,
+//     maxRetries: 3,
+//     logPath: "./logs/",
+//   };
+//   const scraper = new Scraper(config); //Create a new Scraper instance, and pass config to it.
+//   const root = new Root();
+//   const status = new CollectContent("a", {
+//     name: "status",
+//     getPageObject,
+//   });
+//   root.addOperation(status);
+//   await scraper.scrape(root);
+//   fs.writeFile("./test.json", JSON.stringify(pages), () => {});
+// })();
+// }
+
+// const nightmare = Nightmare({
+//   show: true, // will show browser window
+//   openDevTools: true, // will open dev tools in browser window
+// });
+
+// const selector = "td";
+
+// nightmare
+//   .goto(url)
+//   .wait(selector)
+//   .evaluate((selector) => {
+//     return {
+//       nextPage: document.querySelector(selector),
+//     };
+//   }, selector)
+//   .then((extracted) => {
+//     console.log(extracted.nextPage); //Your extracted data from evaluate
+//   });
